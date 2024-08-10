45d420189539f2ad031111e119ff7a87fe2037ef
jzimeri123 <jzimeri@gmail.com>
2024-08-10 07:13:55 -0400

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,12 +2,9 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("hello world");
-        System.out.println("this si the project version 2");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("this si the project version 3");
 
-        //done with version two
+
+        //done with version three
     }
 }
